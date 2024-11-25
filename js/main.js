b15ef9d8370ec9d67b5b907e1202ff20e442dc11
Maihetemy <maihetemy@gmail.com>
2024-11-25 22:48:07 +0200

    clear function and localStorage getItem

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@ var siteURLInput = document.getElementById('siteURLInput');
 var siteSubmitBtn = document.getElementById('siteSubmitBtn');
 var sitesList  =[];
 
+sitesList = JSON.parse(localStorage.getItem('site'));
 // localStorage.getItem
 
+// add function
 function addSite(){
     var siteObj = {
         siteName : siteNameInput.value,
@@ -15,4 +17,12 @@ function addSite(){
     sitesList.push(siteObj);
     localStorage.setItem('site', JSON.stringify(sitesList));
     console.log(sitesList);
+    clearSite();
+}
+
+// clear function
+
+function clearSite(){
+    siteNameInput.value = null;
+    siteURLInput.value = null;
 }
