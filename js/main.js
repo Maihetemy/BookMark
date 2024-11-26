85649bb0716ab20e53ed6027a61af16091058c2a
Maihetemy <maihetemy@gmail.com>
2024-11-26 05:51:50 +0200

    delete function

--- js/main.js
+++ js/main.js
@@ -7,55 +7,66 @@ var siteURLInput = document.getElementById('siteURLInput');
 var siteSubmitBtn = document.getElementById('siteSubmitBtn');
 var sitesList = [];
 
-if( JSON.parse(localStorage.getItem('sites')) ){
+if (localStorage.getItem('sites')) {
     sitesList = JSON.parse(localStorage.getItem('sites'));
-    displaySites();
+    displaySites(sitesList);
 }
 
 // add function
-function addSite(){
+function addSite() {
     var siteObj = {
-        siteId : Date.now,
-        siteName : siteNameInput.value,
-        siteURL : siteURLInput.value,
+        siteId: Date.now(),
+        siteName: siteNameInput.value,
+        siteURL: siteURLInput.value,
     }
+    console.log(siteObj.siteURL);
+    
     sitesList.push(siteObj);
     localStorage.setItem('sites', JSON.stringify(sitesList));
-    console.log(sitesList);
-    displaySites();
+    displaySites(sitesList);
     clearSite();
 }
 
 // clear function
 
-function clearSite(){
+function clearSite() {
     siteNameInput.value = null;
     siteURLInput.value = null;
 }
 
 // display function
-function displaySites(){
-    console.log(sitesList.length);
+function displaySites(list) {
     var box = ``;
-    for(var i = 0; i<sitesList.length; i++){        
-        box += `<div class="col-3 text-center">
-                    <p>${i+1}</p>
-                </div>
-                <div class="col-3 text-center">
-                    <p>${sitesList[i].siteName}</p>
-                </div>
-                <div class="col-3 text-center">
-                    <a href="${sitesList[i].siteURL}" target="_blank">
-                        <button class="bg-warning border-0 py-2 px-3 text-white rounded-2"><i class="fa-solid fa-eye"></i>
-                            Visit</button>
-                    </a>
-                </div>
-                <div class="col-3 text-center">
-                    <button class="bg-danger border-0 py-2 px-3 text-white rounded-2"><i
-                            class="fa-solid fa-trash-can"></i>
-                        Delete</button>
-                </div>`;
+    for (var i = 0; i < list.length; i++) {
+        box += `<div class="row">
+                        <div class="col-3 text-center">
+                            <p>${i + 1}</p>
+                        </div>
+                        <div class="col-3 text-center">
+                            <p>${list[i].siteName}</p>
+                        </div>
+                        <div class="col-3 text-center">
+                            <a href="${list[i].siteURL}" target="_blank">
+                                <button class="bg-warning border-0 py-2 px-3 text-white rounded-2"><i class="fa-solid fa-eye"></i>
+                                    Visit</button>
+                            </a>
+                        </div>
+                        <div class="col-3 text-center">
+                            <button onclick="deleteSite(${list[i].siteId});" class="bg-danger border-0 py-2 px-3 text-white rounded-2"><i
+                                    class="fa-solid fa-trash-can"></i>
+                                Delete</button>
+                        </div>
+                    </div>`;
     }
-    console.log(box);
     linksTable.innerHTML = box;
+}
+
+// delete function
+function deleteSite(id) {
+
+    sitesList = sitesList.filter(function (ele) { return ele.siteId !== id; });
+
+    localStorage.setItem('sites', JSON.stringify(sitesList));
+
+    displaySites(sitesList);
 }
